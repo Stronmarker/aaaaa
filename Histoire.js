78a6251330e8fb67a1c5e0574b23a7e7fa14df88
Stronmarker <Pika20k@gmail.com>
2024-05-14 13:59:43 +0200

    correction bug des fichiers

---
+++ Histoire.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function HistoireScreen() {
+  return (
+    <View>
+      <Text>Parcours Screen</Text>
+    </View>
+  );
+}
+
+export default HistoireScreen;
